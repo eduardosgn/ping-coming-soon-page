5c54a1aebb3991c3cacf9618d2bd347a87058dbd
unknown <you@example.com>
2022-01-31 10:17:48 -0300

    Estilização e animações inseridas no componente Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,8 @@
+import { motion } from 'framer-motion';
+
 import styles from './style.module.scss';
 import PingLogo from '../../public/logo.svg';
+import DashboardImage from '../../public/illustration-dashboard.png';
 
 type TitleHeader = {
     title: string;
@@ -8,17 +11,53 @@ type TitleHeader = {
 
 export default function Header(props: TitleHeader) {
     return (
-        <div className={ styles.container }>
+        <motion.section className={ styles.container }>
             <img src={ PingLogo } alt="Ping" />
-            <h1>{ props.title }</h1>
-            <p>{ props.subtitle }</p>
+            <motion.h1
+                initial='titleInit'
+                animate='titleAnim'
+                variants={{
+                    titleInit: {
+                        opacity: 0,
+                        y: 30
+                    },
+                    titleAnim: {
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            duration: .6,
+                        }
+                    }
+                }}
+            >
+                { props.title }
+            </motion.h1>
+            <motion.p
+                initial='paraInit'
+                animate='paraAnim'
+                variants={{
+                    paraInit: {
+                        opacity: 0,
+                        y: -20
+                    },
+                    paraAnim: {
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                            duration: .8,
+                        }
+                    }
+                }}
+            >
+                { props.subtitle }
+            </motion.p>
 
-            <div className={ styles.emailForm }>
-                <form>
-                    <input type="email" name="email" id="email" placeholder="Your e-mail address..." />
-                    <button type="submit">Notify me</button>
-                </form>
-            </div>
-        </div>
+            <form>
+                <input type="email" name="email" id="email" placeholder="Your e-mail address..." />
+                <button type="submit">Notify me</button>
+            </form>
+
+            <img src={ DashboardImage } alt="Dashboard Illustration" className={ styles.dashImg } />
+        </motion.section>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { motion } from 'framer-motion';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <motion.div
+      initial='initPage'
+      animate='pageAnim'
+      variants={{
+        initPage: {
+          opacity: 0,
+        },
+        pageAnim: {
+          opacity: 1,
+          transition: {
+            duration: .5
+          }
+        }
+      }}
+    >
+      <App />
+    </motion.div>
   </React.StrictMode>,
   document.getElementById('root')
 )
